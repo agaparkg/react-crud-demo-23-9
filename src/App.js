ad7c477880671921807177a0179ff3756cf8fa6e
Azamat Gaparov <agaparovkg@gmail.com>
2024-01-11 17:30:17 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, InputGroup, InputGroupText, Input } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Students CRUD App</h1>
+      <InputGroup className="w-50 my-2 mx-auto">
+        <Button color="danger">x</Button>
+        <Input placeholder="Search..." />
+        <Button color="primary">Add New Student</Button>
+      </InputGroup>
     </div>
   );
 }
